1a0a430c3143651b7b06dc152531dc56e68d5c88
kobiyashimaru2030 <abdulgafar.jagun@andela.com>
2018-10-31 17:21:14 +0100

    added and modified request_handler.js

---
+++ server/request_handler.js
@@ -0,0 +1,31 @@
+
+
+//handle route requests should take in request data, handlers, routes, req and res
+function handleRequests(requestData, handlers, routes, req, res){
+
+    var chosenHandler = typeof(routes[requestData.path]) !== 'undefined' ? routes[requestData.path] : handlers.notFound;
+
+    chosenHandler(requestData, function(statusCode, payload){
+
+            //validate statusCode
+            statusCode = typeof(statusCode) == 'number' ? statusCode : 200;
+
+            //validate payload
+            payload = typeof(payload) == 'object' ? payload : {};
+
+            //convert payload to string
+            var payloadString = JSON.stringify(payload);
+
+            //return a reponse
+            res.setHeader('Content-Type', 'Application/json');
+            res.writeHead(statusCode);
+            res.end(payloadString);
+
+            //log requested route
+            console.log('requested route info: ', statusCode, payload);
+    })
+};
+
+
+
+module.exports = handleRequests;
