73eb539d638d9c8abbb171de6b454938220dc31d
kobiyashimaru2030 <abdulgafar.jagun@andela.com>
2018-11-14 15:54:53 +0100

    Implemented handler.get, handler.put, handler.post and modified error messages to return json objects

--- handlers/handler.js
+++ handlers/handler.js
@@ -42,31 +42,81 @@ handlers.acronymMethods.post = function(data, callback) {
         //check if acronym already exists
         _data.read('acronym', model.acronym, function(err, data){
             if(!err){
-                callback(400, 'this acronym already exists');
+                callback(400, {"Error" : "this acronym already exists"});
             } else {
                 _data.create('acronym', model.acronym, model, function(err){
                     if(!err){
                         callback(200);
                     } else {
                         console.log(err);
-                        callback(500, { 'Error': 'could not create a new acronym'});
+                        callback(500, { "Error": "could not create a new acronym"});
                     }
                 });
             }
         });
 
     } else {
-        callback(400, { 'Error': 'this is not a valid acronym object'});
+        callback(400, { "Error": "this is not a valid acronym object"});
     }
 };
 
 //GET
 handlers.acronymMethods.get = function(data, callback) {
 
+    var acronym = typeof(data.queryStringObject.acronym) == 'string' ? data.queryStringObject.acronym.trim() : false;
+    
+    console.log(acronym);
+
+    if(acronym){
+        _data.read('acronym', acronym, function(err, data){
+            if(!err && data){
+                callback(200, data);
+            } else {
+                callback(err, {'Error': 'was not able get data'});
+            }
+        });
+    } else {
+        callback(404);
+    }
 };
 
 //PUT
 handlers.acronymMethods.put = function(data, callback) {
+
+    var _ = data.payload;
+    var model = new acronym(
+        _.acronym,
+        _.meaning,
+        _.description,
+        _.pioneer,
+        _.date,
+    );
+    
+
+    if(model.acronym && model.meaning && model.description && model.pioneer && model.date){
+        _data.read('acronym', _.acronym, function(err, data){
+            if(!err && data){
+                _data.delete('acronym', _.acronym, function(err){
+                    if(!err){
+                        _data.create('acronym', model.acronym, model, function(err){
+                            if(!err){
+                                callback(200);
+                            } else {
+                                console.log(err);
+                                callback(500, { "Error": "could not update acronym"});
+                            }
+                        });
+                    } else {
+                        callback(err);
+                    }
+                });
+            } else {
+                callback(err);
+            }
+        });
+    } else {
+        callback(400, { 'Error': 'incomplete update data'});
+    }
 
 };
 
